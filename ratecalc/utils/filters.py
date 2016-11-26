97a228585113b8d9664e7a4e0022f5f3feb697c1
Ulrich Feindt <ulrich.feindt@fysik.su.se>
2016-11-26 16:36:40 +0100

    filter dir

--- ratecalc/utils/filters.py
+++ ratecalc/utils/filters.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 import sncosmo
 
+from ratecalc_django.settings import BASE_DIR
+
 def load_filters():
-    filter_dir = 'ratecalc/utils/filters'
+    filter_dir = os.path.join(BASE_DIR, 'ratecalc/utils/filters')
     for filename in os.listdir(filter_dir):
         data = np.genfromtxt('%s/%s'%(filter_dir, filename))
         name = filename.split('.')[0]
