31a8078ba139c6cc67f44fdae7e2181c11db1955
Ulrich Feindt <ulrich.feindt@fysik.su.se>
2016-11-28 00:22:39 +0100

    remove debug

--- ratecalc/templatetags/ratecalc_template_tags.py
+++ ratecalc/templatetags/ratecalc_template_tags.py
@@ -6,10 +6,7 @@ register = template.Library()
 @register.inclusion_tag('ratecalc/transient_models.html')
 def get_tm_list(ttype=None):
     if ttype is None:
-        print 'none'
-        print {'transient_models': TransientModel.objects.all()}
         return {'transient_models': TransientModel.objects.all()}
     else:
-        print 'not none'
         return {'transient_models':
                 TransientModel.objects.filter(transient_type=ttype)}
