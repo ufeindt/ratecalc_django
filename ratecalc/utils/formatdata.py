cac5a532dbae3c3ad99c0466285343707beeaf0b
Ulrich Feindt <ulrich.feindt@fysik.su.se>
2016-11-30 08:51:40 -0800

    adjusted lc time output

--- ratecalc/utils/formatdata.py
+++ ratecalc/utils/formatdata.py
@@ -1,5 +1,6 @@
 def format_lightcurve_data(phase, mags, labels):
-    format_str = '   '.join(['{:>7.2f}' for k in range(len(mags) + 1)])
+    format_str = '{:>8.3f}   '
+    format_str += '   '.join(['{:>7.2e}' for k in range(len(labels))])
     out = ['# time    ' + '    '.join(labels)]
     for k in xrange(len(phase)):
         data = [phase[k]] + [m[k] for m in mags]
@@ -21,7 +22,7 @@ def format_expected_data(mag, n, labels):
         
 def format_redshift_data(z, n):
     format_str = '{:>5.3f}   {:>7.2e}'
-    out = ['# z_bincenter    n_transient']
+    out = ['# z_bincenter n_transient']
     for k in xrange(len(z)):
         out.append(format_str.format(z[k], n[k]))
 
